85ff1cf4a4f0f3be7faa7a68f4dbf2970929194a
oslab1 <oslab1@linkhua.com>
2017-05-26 23:08:52 -0400

    Add files via upload

---
+++ template/linkhua/common/scripts.js
@@ -0,0 +1,13 @@
+﻿//选择器
+jQuery("document").ready(function($){
+var pos = $('.n5_dhys').offset().top;
+var nav = $('.n5_dhys');
+
+$(window).scroll(function () {
+if ($(this).scrollTop() > pos) {
+nav.addClass("f-n5_dhys");
+} else {
+nav.removeClass("f-n5_dhys");
+}
+});
+});
